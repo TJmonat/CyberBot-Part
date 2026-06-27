5eb1553cce3da9ec715cde1d35a9c100985f0005
TJmonat <ST10481840@rcconnect.edu.za>
2026-06-27 17:04:18 +0200

    Add files via upload
    added task assistant

---
+++ Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POE2
+{
+   public class Task
+    {//we are using this class to get the taskid, title, details, reminddate and status of the tasks
+        public int TaskID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Reminder { get; set; }
+        public string Status { get; set; }
+    }
+}
